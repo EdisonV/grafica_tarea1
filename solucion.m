e6e314d464056b74e69a5e3a45006b7781e1b815
EdisonV <evt@hotmail.cl>
2016-10-05 05:16:01 -0400

    Update and rename tarea.m to solucion.m

--- solucion.m
+++ solucion.m
@@ -4,22 +4,20 @@ iter=2000;
 [n, ~]=size(g);
 
 p=1/dxy;
-gxy=n*p; %numero de puntos en x e y
-%parametro de sobrerrelajacion sucesiva
+gxy=n*p; %numero de puntos por lado
+%cambio de medidas para evitar valores NaN
 if dxy<=1
     gxy=5*gxy;
     dxy=5*dxy;
-    iter=500;
+    iter=100000;
     p=5*p;
-%     in=10*in;
-%     out=10*out;
 end
-w=4.0/(2.0+sqrt(4.0-(cos(pi/(gxy-1))+cos(pi/(gxy-1)))^2))
-error=0.0001;
-err=1;
+%parametro de sobrerrelajacion sucesiva
+w=4.0/(2.0+sqrt(4.0-(cos(pi/(gxy-1))+cos(pi/(gxy-1)))^2));
 count=0;
 inxy=[];
 outxy=[];
+%calculando coordenadas de entrada y salida
 for i=1:n
     if g(1,i)~=0
         inxy=[1 i 1];
@@ -34,9 +32,8 @@ for i=1:n
         outxy=[i n 4];
     end
 end
-inxy
-outxy
 m=zeros(gxy);
+%discretizar laberinto
 for i=0:n-1
     for j=0:n-1
         for k=1:p
@@ -46,13 +43,27 @@ for i=0:n-1
         end
     end
 end
-m1=m;
+m1=m;%copia de la matriz para rescatar diferencia de murallas y pasillos
 while count<iter
-    count=count+1
+    count=count+1;
     for i=1:gxy
         for j=1:gxy
             if m1(i,j)~=0 && i>1 && j>1 && i<gxy && j<gxy
-                r=(((m(i+1,j)+m(i-1,j)-2*m(i,j))/(dxy^2))+((m(i,j+1)+m(i,j-1)-2*m(i,j))/(dxy^2)))/4;
+                %condiciones de neumann por murallas
+                arr=m(i+1,j); abj=m(i-1,j); der=m(i,j+1); izq=m(i,j-1);
+                if m1(i+1,j)==0
+                    arr=m(i-1,j);
+                elseif m1(i-1,j)==0;
+                    abj=m(i+1,j);
+                end
+                if m1(i,j+1)==0
+                    der=m(i,j-1);
+                elseif m1(i,j-1)==0
+                    izq=m(i,j+1);
+                end
+                %calculo del punto (x,y)
+                r=(((arr+abj-2*m(i,j))/(dxy^2))+((der+izq-2*m(i,j))/(dxy^2)))/4;
+            %condiciones de neumann en la entrada y salida
             elseif i<=p*inxy(1) && i>p*inxy(1)-p && j<=p*inxy(2) && j>p*inxy(2)-p && inxy(3)==1
                 r=(((2*m(i-1,j)+2*in*dxy-2*m(i,j))/(dxy^2))+((m(i,j+1)+m(i,j-1)-2*m(i,j))/(dxy^2)))/4;
             elseif i<=p*inxy(1) && i>p*inxy(1)-p && j<=p*inxy(2) && j>p*inxy(2)-p && inxy(3)==2
@@ -62,13 +73,14 @@ while count<iter
             elseif i<=p*outxy(1) && i>p*outxy(1)-p && j<=p*outxy(2) && j>p*outxy(2)-p && outxy(3)==4
                 r=(((m(i+1,j)+m(i-1,j)-2*m(i,j))/(dxy^2))+((2*m(i,j-1)+out*2*dxy-2*m(i,j))/(dxy^2)))/4;
             end
+            %condicion de dirichlet en murallas
             if m1(i,j)~=0
                 m(i,j)=m(i,j)+w*r;
-                err=abs(r);
             end
         end
     end
 end
+%dibujo
 figure
 imagesc(m);
 end
