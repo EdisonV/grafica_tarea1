d87f32c7291983a972d4f3c870e3d2c2730b25d1
EdisonV <evt@hotmail.cl>
2016-10-07 20:51:48 -0400

    Update solucion.m

--- solucion.m
+++ solucion.m
@@ -1,16 +1,18 @@
 function m = solucion (g,dxy,in,out)
 g=((in+out)/2)*g;
-iter=2000;
+iter=1000;
 [n, ~]=size(g);
-
+u=2; %factor para cambio de unidades
 p=1/dxy;
 gxy=n*p; %numero de puntos por lado
 %cambio de medidas para evitar valores NaN
-if dxy<=1
-    gxy=5*gxy;
-    dxy=5*dxy;
-    iter=100000;
-    p=5*p;
+if dxy<1
+    in=u*in;
+    out=u*out;
+    gxy=u*gxy;
+    dxy=u*dxy;
+    iter=u*11000;
+    p=u*p;
 end
 %parametro de sobrerrelajacion sucesiva
 w=4.0/(2.0+sqrt(4.0-(cos(pi/(gxy-1))+cos(pi/(gxy-1)))^2));
@@ -32,13 +34,19 @@ for i=1:n
         outxy=[i n 4];
     end
 end
+inxy1=[1 n-2 1];
+g1=g;
+g1(1,n-2)=1;
+g1(2,n-2)=1;
 m=zeros(gxy);
+h=m;
 %discretizar laberinto
 for i=0:n-1
     for j=0:n-1
         for k=1:p
             for l=1:p
                 m(p*i+k,p*j+l)=g(i+1,j+1);
+                h(p*i+k,p*j+l)=g1(i+1,j+1);
             end
         end
     end
@@ -51,25 +59,31 @@ while count<iter
             if m1(i,j)~=0 && i>1 && j>1 && i<gxy && j<gxy
                 %condiciones de neumann por murallas
                 arr=m(i+1,j); abj=m(i-1,j); der=m(i,j+1); izq=m(i,j-1);
-                if m1(i+1,j)==0
+                if i+1>gxy || m1(i+1,j)==0
                     arr=m(i-1,j);
-                elseif m1(i-1,j)==0;
+                elseif i-1<1 || m1(i-1,j)==0
                     abj=m(i+1,j);
                 end
-                if m1(i,j+1)==0
+                if j+1>gxy || m1(i,j+1)==0
                     der=m(i,j-1);
-                elseif m1(i,j-1)==0
+                elseif j-1<1 || m1(i,j-1)==0
                     izq=m(i,j+1);
                 end
                 %calculo del punto (x,y)
-                r=(((arr+abj-2*m(i,j))/(dxy^2))+((der+izq-2*m(i,j))/(dxy^2)))/4;
+                if (m1(i+1,j)~=0 || m1(i-1,j)~=0) && (m1(i,j+1)~=0 || m1(i,j-1)~=0)
+                    r=(((arr+abj-2*m(i,j))/(dxy^2))+((der+izq-2*m(i,j))/(dxy^2)))/4;
+                elseif (m1(i+1,j)==0 && m1(i-1,j)==0)
+                    r=((der+izq-2*m(i,j))/(dxy^2))/4;
+                elseif (m1(i,j+1)==0 && m1(i,j-1)==0)
+                    r=((arr+abj-2*m(i,j))/(dxy^2))/4;
+                end
             %condiciones de neumann en la entrada y salida
             elseif i<=p*inxy(1) && i>p*inxy(1)-p && j<=p*inxy(2) && j>p*inxy(2)-p && inxy(3)==1
-                r=(((2*m(i-1,j)+2*in*dxy-2*m(i,j))/(dxy^2))+((m(i,j+1)+m(i,j-1)-2*m(i,j))/(dxy^2)))/4;
+                r=(((2*m(i+1,j)+2*in*dxy-2*m(i,j))/(dxy^2))+((m(i,j+1)+m(i,j-1)-2*m(i,j))/(dxy^2)))/4;
             elseif i<=p*inxy(1) && i>p*inxy(1)-p && j<=p*inxy(2) && j>p*inxy(2)-p && inxy(3)==2
                 r=(((m(i+1,j)+m(i-1,j)-2*m(i,j))/(dxy^2))+((2*m(i,j+1)+in*2*dxy-2*m(i,j))/(dxy^2)))/4;
             elseif i<=outxy(1)*p && i>p*outxy(1)-p && j<=p*outxy(2) && j>p*outxy(2)-p && outxy(3)==3
-                r=(((2*m(i+1,j)+2*out*dxy-2*m(i,j))/(dxy^2))+((m(i,j+1)+m(i,j-1)-2*m(i,j))/(dxy^2)))/4;
+                r=(((2*m(i-1,j)+2*out*dxy-2*m(i,j))/(dxy^2))+((m(i,j+1)+m(i,j-1)-2*m(i,j))/(dxy^2)))/4;
             elseif i<=p*outxy(1) && i>p*outxy(1)-p && j<=p*outxy(2) && j>p*outxy(2)-p && outxy(3)==4
                 r=(((m(i+1,j)+m(i-1,j)-2*m(i,j))/(dxy^2))+((2*m(i,j-1)+out*2*dxy-2*m(i,j))/(dxy^2)))/4;
             end
@@ -80,7 +94,9 @@ while count<iter
         end
     end
 end
+m=m/u;
 %dibujo
 figure
 imagesc(m);
 end
+ 
